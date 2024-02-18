156282a8d2184c4bbfc85386b8c70ef8c2df08a1
Abduraufdeveloper <abhuwaja@gmail.com>
2024-02-18 14:41:45 +0500

    fix

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-lst = [1,2,3,4,5,6,7]
+#lst = [1,2,3,4,5,6,7]
 #for x in lst:
 #    if x == 5 :
 #        continue
 #    print(x)
 
-i = 0
-while i < len(lst):
+#i = 0
+#while i < len(lst):
 
-    i += 1
-    if i == 5 :
-        continue
-    print(i)
-while lst:
-    x = lst.pop(0)
-    print(x)
-else:
-    print("list mavjud emas !")
+#    i += 1
+#    if i == 5 :
+#        continue
+#    print(i)
+#while lst:
+#    x = lst.pop(0)
+#    print(x)
+#else:
+#    print("list mavjud emas !")
 
-for x in lst:
-    print(x)
+#for x in lst:
+#    print(x)
+
+
+def lst():
+    return lst()
+
+print(lst())
