d83f2745b8c5af8a2f99b6d4819e0c26a0e70dbf
Abduraufdeveloper <abhuwaja@gmail.com>
2024-02-18 14:13:52 +0500

    fix

---
+++ main.py
@@ -0,0 +1,39 @@
+lst = [1,2,3,4,5,6,7]
+#for x in lst:
+#    if x == 5 :
+#        continue
+#    print(x)
+
+i = 0
+while i < len(lst):
+    x = lst[i]
+    if x == 5 :
+        continue
+    print(x)
+    i += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
